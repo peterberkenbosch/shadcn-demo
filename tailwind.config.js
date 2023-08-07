ba85f5e9a48d998e2ccfef0a6d3a683a19d2e5fe
Peter Berkenbosch <info@peterberkenbosch.nl>
2023-08-07 20:47:03 +0200

    Add shadcn.tailwind.js config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const shadcnConfig = require('./config/shadcn.tailwind')
+
 module.exports = {
   plugins: [
-    require('@tailwindcss/forms')({ strategy: 'class' }),
+    require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/container-queries'),
   ],
+  ...shadcnConfig,
 
   content: [
     './app/helpers/**/*.rb',
